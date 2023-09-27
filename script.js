6803f8da0b6774f1ffa818bf3b490d02f44a1595
Jappe02 <69361903+Jappe02@users.noreply.github.com>
2023-09-27 18:48:16 +0300

    Test

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Define your page content here
+const pages = {
+    firstpage: "<h1>First Page</h1><p>This is the content of the first page.</p>",
+    secondpage: "<h1>Second Page</h1><p>This is the content of the second page.</p>",
+};
+
+// Function to update the content of the 'div' element
+function updateContent(pageName) {
+    const contentDiv = document.getElementById("content");
+
+    if (pages.hasOwnProperty(pageName)) {
+        contentDiv.innerHTML = pages[pageName];
+    } else {
+        contentDiv.innerHTML = "<h1>Page not found</h1>";
+    }
+}
+
 // Function to handle navigation and update content
 function navigateToPage(pageName) {
     updateContent(pageName);
@@ -5,6 +22,10 @@ function navigateToPage(pageName) {
     // Push a state to the history API
     history.pushState({ page: pageName }, "", `/${pageName}`);
 }
+
+// Handle initial page load
+const initialPage = window.location.pathname.replace("/", "");
+navigateToPage(initialPage);
 
 // Handle clicks on "first page" and "second page" links
 document.getElementById("firstPage").addEventListener("click", (event) => {
@@ -22,13 +43,3 @@ window.addEventListener("popstate", (event) => {
     const pageName = event.state?.page || initialPage;
     updateContent(pageName);
 });
-
-// Function to update the content of the 'div' element
-function updateContent(pageName) {
-    const contentDiv = document.getElementById("content");
-    contentDiv.innerHTML = pages[pageName] || "Page not found";
-}
-
-// Handle initial page load
-const initialPage = window.location.pathname.replace("/", "");
-updateContent(initialPage);
